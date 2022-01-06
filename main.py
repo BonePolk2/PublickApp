0d7da92a1771743b76c69026d6241765de3fcebb
* <danilbondare362@gmail.com>
2022-01-06 20:32:43 +0500

    1

---
+++ main.py
@@ -0,0 +1,12 @@
+from kivy.app import App
+from kivy.uix.label import Label
+
+class MyApp(App):
+	
+	def build(self):
+		label = Label(text = 'Hello world!')
+		
+		return label
+		
+Apk = MyApp()
+Apk.run()
